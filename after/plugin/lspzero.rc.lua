3395a5018a7bdb3392bdba28e6656e97caafe861
Michael <michael.lee.farringer@gmail.com>
2023-04-01 13:09:05 +0900

    Saving current changes.

---
+++ after/plugin/lspzero.rc.lua
@@ -0,0 +1,11 @@
+local status, lsp = pcall(require, 'lspconfig')
+if (not status) then return end
+
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+lsp.setup()
--- lua/base.lua
+++ lua/base.lua
@@ -14,7 +14,6 @@ vim.opt.backup = false
 vim.opt.showcmd = true
 vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
-vim.opt.expandtab = true
 vim.opt.scrolloff = 10
 vim.opt.shell = 'fish'
 vim.opt.backupskip = { '/tmp/*', '/private/tmp/*' }
@@ -22,11 +21,16 @@ vim.opt.inccommand = 'split'
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.opt.smarttab = true
 vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
 vim.opt.wrap = false -- No Wrap lines
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
+
+-- Tab set to two spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
 vim.opt.wildignore:append { '*/node_modules/*' }
 
 -- Undercurl
